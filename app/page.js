67edc9ba42bcac1cb43330912c4c8b28cc59e9a9
PariBainsla <paribainsla161@gmail.com>
2024-09-08 20:30:33 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,76 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const page = () => {
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+const [task, settask] = useState("")
+const [maintask, setmaintask] = useState([])
+
+const submitHandler = (e) => {
+  e.preventDefault()
+  setmaintask([...maintask, {task}])
+  console.log(maintask)
+  settask("")
 }
+
+const deleteHandler = (i) => {
+  let copytask = [...maintask]
+  copytask.splice(i,1)
+  setmaintask(copytask)
+}
+
+let renderTask = <h2>No Task Available</h2>
+
+if(maintask.length>0){
+  renderTask = maintask.map((t,i) => {
+    return(
+      <li key={i} className='flex items-center justify-between '>
+        <div className='m-5'>
+          <h5>{t.task}</h5>
+        </div>
+        <button onClick={()=>{
+          deleteHandler(i)
+        }} className='bg-red-500 text-white rounded px-4 py-1 '>Delete</button>
+        
+      </li>
+      
+    )
+  })
+}
+
+  return (
+
+    <>
+    <div>
+      <h1 className='bg-slate-500 text-5xl p-5 text-center font-bold'>Todo List</h1>
+
+      <form onSubmit={submitHandler}>
+        <input 
+        type='text'
+        className='m-5 px-6 py-3 rounded text-black text-2xl bg-slate-200'
+        placeholder='Enter Task Here'
+        value={task}
+        onChange={(e)=>{
+          settask(e.target.value)
+        
+        }}
+        />
+<button className='bg-slate-500 py-2.5 px-3 rounded font-bold text-2xl'>Add Task</button>
+
+      </form>
+      
+<hr/>
+
+<div className='p-3 m-5 bg-slate-300 text-black '>
+  <ul>
+    {renderTask}
+  </ul>
+</div>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default page
